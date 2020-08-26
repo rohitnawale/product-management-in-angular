4abfb31b15d7672d8c96236aafc2244269836f75
Rohit Nawale <40070862+rohitnawale@users.noreply.github.com>
2020-08-26 20:28:48 +0530

    5 star ratings for products

---
+++ src/app/shared/convert-to-spaces.pipe.ts
@@ -0,0 +1,13 @@
+// import { pipeline } from "stream";
+
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: 'convertToSpaces'
+})
+
+export class ConvertToSpacesPipe implements PipeTransform{
+    transform(value: string, character: string): string{
+        return value.replace(character, " ");
+    }
+}
